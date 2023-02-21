296ca405f044a859c3c429e25883a2f1268d4b4c
Cheila Bergamini <cheilamaria@gmail.com>
2023-02-21 00:12:29 +0000

    Diagrams

---
+++ Diagrams/tgstars_SQL_script.sql
@@ -0,0 +1,15 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/X6Jzf0
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "tgstars" (
+    "Vmag" FLOAT   NOT NULL,
+    "Plx" FLOAT   NOT NULL,
+    "e_Plx" FLOAT   NOT NULL,
+    "B-V" FLOAT   NOT NULL,
+    "Amag" FLOAT   NOT NULL,
+    "TargetClass" INTEGER   NOT NULL,
+    "StarNam" VARCHAR(20)   NOT NULL
+);
+
